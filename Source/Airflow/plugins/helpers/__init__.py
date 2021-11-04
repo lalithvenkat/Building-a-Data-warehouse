c5c53a8654dedbb7948d9d98adedff52d2bfd9c3
lalithvenkat <lalithvenkat27@gmail.com>
2021-11-04 19:59:47 +0000

    add files

---
+++ Source/Airflow/plugins/helpers/__init__.py
@@ -0,0 +1,7 @@
+from helpers.sql_queries_postgres import PostgresSqlQueries
+from helpers.sql_queries_redshift import RedshiftSqlQueries
+
+__all__ = [
+    'PostgresSqlQueries',
+    'RedshiftSqlQueries'
+]
